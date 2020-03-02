c3fafbfd8f2d9aaba9b390a9bf11891de7c2272d
khang <trinhduykhang1313@gmail.com>
2020-03-02 16:22:15 +0700

    refactor

--- controllers/spider.js
+++ controllers/spider.js
@@ -1,7 +1,7 @@
 var search = require("../services/search");
 
 exports.test = (req, res, next) => {
-  search.query("Lãi suất ngân hàng Techcombank tháng 2 2020 kỳ hạn 3 tháng", result => {
+  search.query("Lãi suất ngân hàng Techcombank tháng 2 2020 \"kỳ hạn 3 tháng\"", result => {
     res.render("index", { title: "Express", result: result });
   });
 };
